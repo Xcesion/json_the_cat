1a4fedb97bdd8a6195ca6c9a0e82f26046ca57e8
Frank Shen <frank851207@gmail.com>
2023-06-09 19:29:34 -0700

    breed fetcher test upload the description didnt match up what we expected but it did catch up our error

--- test/breedFetcherTest.js
+++ test/breedFetcherTest.js
@@ -11,11 +11,20 @@ describe('fetchBreedDescription', () => {
 
       const expectedDesc = 
       "The Siberians dog like temperament and affection makes the ideal lap cat and will live quite happily indoors. Very agile and powerful, the Siberian cat can easily leap and reach high places, including the tops of refrigerators and even doors.";
-
       // compare returned description
       assert.equal(expectedDesc, description.trim());
-
-      // done();
+      done();
+    });
+  });
+  it('returns a [] when the breedname is invalid', (done) => {
+    fetchBreedDescription('frank', (err, description) => {
+      // we expect no error for this scenario
+      assert.equal(err, null);
+      const expectedDesc = 
+      "Please enter a breed name after the filename";
+      // compare returned description
+      assert.equal(expectedDesc, description.trim());
+      done();
     });
   });
 });
