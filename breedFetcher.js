a7e15a4109358ba670a86507e9520754d5bd5f28
Frank Shen <frank851207@gmail.com>
2023-06-09 19:30:21 -0700

    breeedFetch and index added callback

--- breedFetcher.js
+++ breedFetcher.js
@@ -1,15 +1,20 @@
 const request = require("request");
 const fs = require('fs');
 
-const url = 'https://api.thecatapi.com/v1/breeds/search?q=sib';
+const url = 'https://api.thecatapi.com/v1/breeds/search?q=';
+
 const fetchBreedDescription = (breedName, callback)=> {
-  request(url, (error, response, body) => {
+  request(url+breedName, (error, response, body) => {
     if (error) {
-      console.error('Request failed:', error);
+      callback('Request failed:');
       return;
     }
     const data = JSON.parse(body);
     console.log(data[0].description);
+    if (data.length === 0) {
+      return callback("Please enter a breed name after the filename");
+    }
+    callback(null, data[0].description);
     // console.log(typeof data);
   });
 }
--- index.js
+++ index.js
@@ -1,13 +1,9 @@
 const { fetchBreedDescription } = require('./breedFetcher');
 
-const args = process.argv.slice(2);
-const breedName = process.argv[2];
-if (args.length === 0) {
-  console.log("Please enter a breed name after the filename");
-  return;
-}
+let args = process.argv.slice(2);
 
-fetchBreedDescription(breedName, (error, description)=>{
+
+fetchBreedDescription(args, (error, description)=>{
   if (error) {
     console.error('Error retrieving breed description:', error);
     return;
